002d4bb309fa0e3821802df390d0a7b62f54b7e4
Santiago Ems <sems@itba.edu.ar>
2026-06-05 12:42:01 -0300

    update logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,14 @@ export const metadata: Metadata = {
   title: "Finza",
   description: "Tu app de finanzas personales",
   manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    title: "Finza",
+    statusBarStyle: "black-translucent",
+  },
+  icons: {
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export const viewport: Viewport = {
